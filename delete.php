c4a142ad8a38ff86ff346be80dea63f0c461d536
Willsi <Willsi.cooper@googlemail.com>
2018-09-27 13:02:29 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 
 <?php
 	$search= $_POST['search'];
-	$con=mysqli_connect("localhost","root","","pokelist");
+	$con=mysqli_connect("35.233.11.254","root","admin","Pokemon");
 	$records=mysqli_query($con,"Delete from `pokelist` WHERE Name = '$search' ");
 	//header("location:myfriend.php");
 	
